6d33a37e34cd85aec3c386b1fe6ce8177286d5c4
galibhabibullah85 <75194709+galibhabibullah85@users.noreply.github.com>
2020-12-11 19:02:00 +0600

    Add files via upload

---
+++ Extra project(Window10)/js/desktop.js
@@ -0,0 +1,12 @@
+window.onload=()=>{
+	// document.getElementById("welcomeTune").autoplay="true";
+	// document.getElementById("welcomeTune").play();
+}
+
+setTimeout(type,2000);
+
+function type(){
+	document.getElementById("cmd").style.width="715px";
+	document.getElementById("cmd").style.height="375px";
+	setTimeout(typing,400);
+}
